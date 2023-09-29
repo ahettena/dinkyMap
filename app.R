61c49c06e713948ec54a2e1a8c616343b23fb263
ahettena <109176167+ahettena@users.noreply.github.com>
2023-09-29 18:00:12 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,6 +1,4 @@
 library(shiny)
-library(ggplot2)
-#library(leaflet)
 
 ui <- fluidPage(
   plotOutput("plotz")
@@ -8,23 +6,11 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   output$plotz <- renderPlot({
-    #leafletOutput("map")
     
     lat <- 30.368254
     long <- -103.626395
-    plot(lat, long)
-    
-    # output$map <- renderLeaflet({
-    #   leaflet() %>%
-    #     addTiles() %>%
-    #     addProviderTiles(providers$Esri.WorldStreetMap) %>%
-    #     addMarkers(
-    #       lng = long,
-    #       lat = lat
-    #     )
-    # })
-    
-  }
+    plot(lat, long) 
+    }
   )
 }
 
